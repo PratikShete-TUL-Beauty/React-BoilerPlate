938fed14b5ac952904ae76294ef59442fa130553
Pratik Shete <pshete@tataunistore.com>
2023-05-07 23:22:47 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="title"> Welcome to React App</h1>
+            <h4 className="context">This is supposed to be React Boilerplate </h4>
+        </div>
+    );
+};
+
+export default App;
